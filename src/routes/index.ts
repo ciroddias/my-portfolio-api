4e4d20e600affcce53b919d6771aaf80e7fcf32b
Ciro Dias <ciro.dias@hygiabank.com>
2022-06-20 22:43:07 -0300

    feat: authenticate user use case implemented

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import { assetsRoutes } from './assets.routes';
 import { usersRoutes } from './users.routes';
 import { transactionsRoutes } from './transactions.routes';
+import { authenticateRoutes } from './authenticate.routes';
 
 const router = Router()
 
 router.use("/assets", assetsRoutes)
 router.use("/users", usersRoutes)
 router.use("/transactions", transactionsRoutes)
+router.use(authenticateRoutes)
 
 export { router }
