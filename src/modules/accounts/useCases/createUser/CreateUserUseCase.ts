abc674647c9f7882107f357bf53f94d91784b041
Ciro Dias <ciro.dias@hygiabank.com>
2022-06-20 21:07:34 -0300

    feat: password being encrypted

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { hash } from "bcrypt"
 
 @injectable()
 class CreateUserUseCase {
@@ -14,8 +15,10 @@ class CreateUserUseCase {
        if (userAlreadyExists) {
             throw new Error("User already exists!")
         }
+
+        const passwordHash = await hash(password, 8)
         
-        await this.usersRepository.create({ name, email, password })
+        await this.usersRepository.create({ name, email, password: passwordHash })
     }
 
 }
